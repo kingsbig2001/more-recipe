88b947a3ff851cf5ac17c2c94ecd75cebb85b746
Ibeh Kingsley <kingsbig2001@yahoo.ca>
2017-09-29 04:00:49 +0100

    added api routes for PUT and Delete http methods

--- app.js
+++ app.js
@@ -24,8 +24,8 @@ app.get('/api/recipe', (req, res) => res.status(200).send(recipe));
 
 //Search recipe using id
 app.get('/api/recipe/:id', function(req, res) {
-    const id = parseInt(req.params.id, 10);
-    const result = recipe.filter(r => r.id === id)[0];
+    let id = parseInt(req.params.id, 10);
+    let result = recipe.filter(r => r.id === id)[0];
  
     if (!result) {
         res.sendStatus(404);
@@ -34,15 +34,49 @@ app.get('/api/recipe/:id', function(req, res) {
     }
 });
 
-app.post('/api/recipes', (req, res)=>{
-    //let recipe = req.body;
-    console.log(req.body);
-    res.send(req.body);
+// app.post('/api/recipe', (req, res)=>{
+//     //let recipe = req.body;
+//     console.log(req.body);
+//     res.send(req.body);
+// });
+app.post('/api/recipe', function(req, res) {
+    let item = req.body;
+ console.log(item.id);
+    if (!item.id) {
+        return res.sendStatus(500);
+    }
+ 
+    recipe.push(item);
+ 
+    res.send(recipe);
 });
 
-app.put('api/recipes', (req, res)=>{
-    app.render(res.json);
+app.put('/api/recipe/:id', function(req, res) {
+    let id = parseInt(req.params.id, 10);
+    let existingItem = recipe.filter(r => r.id === id)[0];
+ 
+    if (!existingItem) {
+        let item = req.body;
+        item.id = id;
+        recipe.push(item);
+        res.setHeader('Location', '/api/recipe/' + id);
+        res.sendStatus(201);
+    } else {
+        existingItem.name = req.body.name;
+        res.sendStatus(204);
+    }
 });
-
+//Delete Method
+app.delete('/app/recipe/:id', function(req, res) {
+    let id = parseInt(req.params.id, 10);
+    let existingItem = recipe.filter(r => r.id === id)[0];
+ 
+    if (!existingItem) {
+        return res.sendStatus(404);
+    }
+ 
+    recipe = recipe.filter(r => r.id !== id);
+    res.sendStatus(204);
+});
 
 module.exports = app;
